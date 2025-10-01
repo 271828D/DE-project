917a1ef3850806e2bf05d5c7d8f00632fef31550
271827D <david.benavente.r@usach.cl>
2025-10-01 23:13:05 +0200

    Added modifications to the main.py. Only the argparse funcionaly to receive the url

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 """Test commit for the main.py"""
+
+import argparse
+
+
+def main():
+    """Main file to run the program"""
+
+    parser = argparse.ArgumentParser(description="ag desc")
+
+    parser.add_argument("url", help="url to use for downloading the .csv file")
+
+    args = parser.parse_args()
+
+    print(f"url to get the .csv file: {args.url}")
+
+    if __name__ == "__main__":
+        main()
