59d4146a802210f0491f281ff5086d0a3f05a9a6
271827D <david.benavente.r@usach.cl>
2025-10-02 18:43:31 +0200

    Read and clean data funcionaloty added to the main.py. Both tested and working locally

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 """Test commit for the main.py"""
 
 import argparse
-from src.download_clean_data.utils.check_url_get_data import GetDataFromURL
+from src.download_clean_data.utils.check_url_get_data import GetDataFromUrl
+from src.download_clean_data.process_data.clean_data import ReadCsv, CleanData
 
 
 def main():
@@ -17,13 +18,27 @@ def main():
 
     print(f"url to get the .csv file: {args.url}")
 
-    # Usando la clase
-    downloader = GetDataFromURL()
-
+    # Get the data
+    downloader = GetDataFromUrl()
     data_content = downloader.check_request(args.url)
+    output_path = downloader.download_file(data_content)  #
 
-    downloader.download_file(data_content)
+    # Read the data from CSV
+    reader = ReadCsv()
+    dataframe = reader.read(output_path)
 
+    # Clean data
+    cleaner = CleanData(dataframe)
+    cleaner.remove_duplicate_rows()
+    cleaner.clean_empty_rows()
+    cleaner.save_discarded_rows()
+
+    # Reports
+
+
+# python main.py
+# --url "https://people.sc.fsu.edu/~jburkardt/data/csv/airtravel.csv"
+# Testing funcionalities
 
 if __name__ == "__main__":
 
