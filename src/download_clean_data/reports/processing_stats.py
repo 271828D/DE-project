1dfd87be91e6aec4a3c372d75147a3dc46cfbd66
271827D <david.benavente.r@usach.cl>
2025-10-02 21:25:45 +0200

    Added class DataStats. This class will calculate the metrics for the first artefact. Class incomplete, need to add the logic to get each metric

---
+++ src/download_clean_data/reports/processing_stats.py
@@ -0,0 +1,27 @@
+"""File with the Class to generate the first artifact"""
+
+import pandas as pd
+
+
+class DataStats:
+    """Class that generate the stats
+    for the first artefact
+    """
+
+    def __init__(self, df: pd.DataFrame):
+        pass
+
+    def get_total_rows():
+        pass
+
+    def get_empty_rows():
+        pass
+
+    def get_total_invalid_discarded_rows():
+        pass
+
+    def get_total_duplicate_rows():
+        pass
+
+    def get_usable_rows():
+        pass
